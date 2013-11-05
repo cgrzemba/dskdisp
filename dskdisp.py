07a91ff1ad726b642829ef30464c65125f1ca2ee
Carsten Grzemba <cgrzemba@opencsw.org>
2013-11-05 08:38:22 +0100

    add license, fix LUN size error, better dev_link extraction/generation

--- dskdisp.py
+++ dskdisp.py
@@ -1,31 +1,31 @@
 #!/usr/bin/env python
-/************************************************************************
- * This tool shows multipathing devices and the corresponding 
- * storage WWN, LUN in a compact manner
- * This informations a extracted from 'prtconf -Dv' so you can use this
- * tool also 'offline' e.g. use explorer output 
- * 
- * Written By: Carsten Grzemba (cgrzemba@opencsw.org)
- * 
- * # CDDL HEADER START
- * #
- * # The contents of this file are subject to the terms of the
- * # Common Development and Distribution License (the "License").
- * # You may not use this file except in compliance with the License.
- * #
- * # You can obtain a copy of the license at pkg/OPENSOLARIS.LICENSE
- * # or http://www.opensolaris.org/os/licensing.
- * # See the License for the specific language governing permissions
- * # and limitations under the License.
- * #
- * # When distributing Covered Code, include this CDDL HEADER in each
- * # file and include the License file at pkg/OPENSOLARIS.LICENSE.
- * # If applicable, add the following below this CDDL HEADER, with the
- * # fields enclosed by brackets "[]" replaced with your own identifying
- * # information: Portions Copyright [yyyy] [name of copyright owner]
- * #
- * # CDDL HEADER END
- ************************************************************************/
+# ************************************************************************
+# * This tool shows multipathing devices and the corresponding 
+# * storage WWN, LUN in a compact manner
+# * This informations a extracted from 'prtconf -Dv' so you can use this
+# * tool also 'offline' e.g. use explorer output 
+# * 
+# * Written By: Carsten Grzemba (cgrzemba@opencsw.org)
+# * 
+# CDDL HEADER START
+#
+# The contents of this file are subject to the terms of the
+# Common Development and Distribution License (the "License").
+# You may not use this file except in compliance with the License.
+#
+# You can obtain a copy of the license at pkg/OPENSOLARIS.LICENSE
+# or http://www.opensolaris.org/os/licensing.
+# See the License for the specific language governing permissions
+# and limitations under the License.
+#
+# When distributing Covered Code, include this CDDL HEADER in each
+# file and include the License file at pkg/OPENSOLARIS.LICENSE.
+# If applicable, add the following below this CDDL HEADER, with the
+# fields enclosed by brackets "[]" replaced with your own identifying
+# information: Portions Copyright [yyyy] [name of copyright owner]
+#
+# CDDL HEADER END
+# ************************************************************************
 
 from sys import exit, argv
 from subprocess import Popen, PIPE
